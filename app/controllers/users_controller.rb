aaa8fc499ea4ecd3a7560d6fca1aca575632ba3b
Daniel Ionita <danielionita88@yahoo.com>
2020-02-03 17:10:18 -0600

    add login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+
+    def login
+      user=User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        token = JWT.encode({id:user.id}, 'secret', 'HS256')
+        render json: {id: user.id, first_name: user.first_name, last_name: user.last_name,token: token}, status: :created
+      else
+        render json: { error: 'failed to login' }, status: :not_acceptable
+      end
+    end
     
     def create
         user = User.create(user_params)
         if user.valid?
-          render json: user, status: :created
+          token = JWT.encode({id:user.id}, 'secret', 'HS256')
+          render json: {id: user.id, first_name: user.first_name, last_name: user.last_name,token: token}, status: :created
         else
           render json: { error: 'failed to create user' }, status: :not_acceptable
         end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   resources :events, only: [:index,:create]
-  post '/auth', to: 'users#create'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'users#login'
 end
